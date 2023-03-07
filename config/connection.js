1c6bc38be3ee067ff560be33f43d2a5f61ec0075
OneDeadFox <tjosephb.pro@gmail.com>
2023-03-07 13:19:00 -0800

    pushing config

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+        port: 3306
+    });
+
+module.exports = sequelize;
