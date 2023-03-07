f38e05defb0062ad210ed5c3f73bde80ba0b6fab
OneDeadFox <tjosephb.pro@gmail.com>
2023-03-07 15:07:06 -0800

    added model index, and note, user, userfriend models

---
+++ models/UserFriend.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../../config/connection');
+
+class UserFriend extends Model { }
+
+UserFriend.primaryKeyAttribute({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "User",
+            key: "id"
+        }
+    },
+    friendId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "User",
+            key: "id"
+        }
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize
+});
+
+module.exports = UserFriend;
