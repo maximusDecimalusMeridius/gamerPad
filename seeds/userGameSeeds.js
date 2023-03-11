76ae58c9e569644ed7351aa48f9f0e19d77a0c9d
Maya Lynn <mayalynncohen@gmail.com>
2023-03-10 16:52:32 -0800

    added platform seeds

--- seeds/userGameSeeds.js
+++ seeds/userGameSeeds.js
@@ -1,51 +1,41 @@
 const {UserGame, Platform} = require('../models');
 
 const UserGamePlatform = async () => {
-    const platformData = await Platform.bulkCreate([
-        {
-            platform: 'Xbox',
-            GameId: 1,
-            UserGameId: 1,
-        },
-        {
-            platform: 'Playstation',
-            GameId: 2,
-            UserGameId: 2,
-        },
-        {
-            platform: 'Discord',
-            GameId: 3,
-            UserGameId: 3,
-        }
-    ]);
     const userGameData = await UserGame.bulkCreate([
         {
             favorite: true,
             lookingForMore: true,
-            content: 10,
-            value: 10,
-            replay: 100,
+            content: 5,
+            value: 4,
+            replay: 5,
             UserId: 1,
             GameId: 1,
         },
         {
             favorite: true,
             lookingForMore: true,
-            content: 10,
-            value: 10,
-            replay: 100,
+            content: 5,
+            value: 5,
+            replay: 5,
             UserId: 2,
             GameId: 2,
         },
         {
             favorite: true,
             lookingForMore: true,
-            content: 10,
-            value: 10,
-            replay: 100,
+            content: 4,
+            value: 3,
+            replay: 4,
             UserId: 1,
             GameId: 2,
         }
     ])
+
+    await userGameData[0].addPlatform(8)
+    await userGameData[0].addPlatform(27)
+
+    await userGameData[1].addPlatform(27)
+
+    await userGameData[2].addPlatform(8)
 }
 module.exports = UserGamePlatform;
