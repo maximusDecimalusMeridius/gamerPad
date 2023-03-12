8364ced1055b0c3a25b953ffcc620408bd73f3e1
Maya Lynn <mayalynncohen@gmail.com>
2023-03-12 11:32:48 -0700

    correction of the word Written

--- controllers/noteRoutes.js
+++ controllers/noteRoutes.js
@@ -25,7 +25,7 @@ router.get("/currentUserNotes", async (req, res) => {
         const tokenData = jwt.verify(token, process.env.JWT_SECRET);
 
         const userData = await User.findByPk(tokenData.id,{include:[
-            {model:Note, as: "WritenNotes", foreignKey: "AuthorId", attributes:["id","title", "textContent", "color", "isShared", "createdAt"]},
+            {model:Note, as: "WrittenNotes", foreignKey: "AuthorId", attributes:["id","title", "textContent", "color", "isShared", "createdAt"]},
             {model:Note, as: "SharedNotes", foreignKey: "SharedId", attributes:["id","title", "textContent", "color", "author", "createdAt"]}], 
             attributes:["id", "username"]});
 
